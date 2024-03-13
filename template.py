952b63f68c637d3df0d666227bf5e3115c55f7fd
vinaykadam007 <vinay0745@gmail.com>
2024-03-12 22:16:40 -0500

    added main.py

--- template.py
+++ template.py
@@ -22,7 +22,7 @@ list_of_files = [
     'params.yaml',
     'requirements.txt',
     'setup.py',
-    'research/trails.pynb',
+    'research/trails.ipynb',
     'templates/index.html',
     'test.py'
 ]   
